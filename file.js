85abf2698e2894c676085cde92983ec185d465d7
Maybol283 <126118241+Maybol283@users.noreply.github.com>
2023-05-18 07:41:47 +0900

    Began adding game logic
    Began adding game logic, need to work out how to select the correct cell next.
    
    Also updated gameboard by removing outerborders

--- file.js
+++ file.js
@@ -1,1 +1,22 @@
-print("Hello")
+//start the game
+//choose which player goes first
+//when player clicks an empty gap add O / X depending on which player
+//Update the board and check for win
+//if no win switch players turn
+//repeat step 3
+//else if (win) display "Winner Player (1/2)"
+
+let player1 = "X";
+let player2 = "O";
+let gameBoard = [
+["", "", "",],
+["", "", "",],
+["", "", "",]
+];
+
+function makeMove (row, col) {
+ if(gameBoard[row][col] == ""){
+    gameBoard[row][col] == currentPlayer
+    document.getElementsByClassName("")
+ }
+}
