fe3d35639c99e0c72bc9bdc4c34ec1d6c77d26c2
Maybol283 <126118241+Maybol283@users.noreply.github.com>
2023-05-20 14:00:04 +0900

    Initial Game Logic Added
    X 'sand O's are produced on click, win condition has not been fully implemented. Although a regex finding the winning condition has been made and is working.

--- file.js
+++ file.js
@@ -1,22 +1,48 @@
-//start the game
-//choose which player goes first
-//when player clicks an empty gap add O / X depending on which player
-//Update the board and check for win
-//if no win switch players turn
-//repeat step 3
-//else if (win) display "Winner Player (1/2)"
+
 
 let player1 = "X";
 let player2 = "O";
 let gameBoard = [
-["", "", "",],
-["", "", "",],
-["", "", "",]
+"U", "U", "U",
+"U", "U", "U",
+"U", "U", "U",
 ];
 
-function makeMove (row, col) {
- if(gameBoard[row][col] == ""){
-    gameBoard[row][col] == currentPlayer
-    document.getElementsByClassName("")
+// Creates tiles 1 - 9
+for (let i = 1; i <= 9; i++) {
+    document.getElementById(i).addEventListener("click", function () {
+      makeMove(i);
+    });
+  }
+
+let playersTurn = 0
+
+//Changes the tile to either X or O
+const  makeMove = tileIndex => {
+    let tile = document.getElementById(tileIndex)
+   
+    if (playersTurn == 0){
+    tile.innerHTML = "X";
+       playersTurn = 1;
+       gameBoard[tileIndex - 1] = "X"
+    } else {
+        tile.innerHTML = "O";
+        playersTurn = 0;
+        gameBoard[tileIndex - 1] = "O"
+    }
+    checkWinner();
  }
+
+ winX = /^X{3}|(...X){3}|(X..){3}|.X..X..X.|(..X){3}|X...X...X|..X.X.X../gm
+ winO = /^O{3}|(...O){3}|(X..){3}|.O..O..O.|(..X){3}|O...O...O|..O.O.O../gm
+
+  function checkWinner (gameBoard) {
+    gameBoard.join("");
+    if (gameBoard.match(winX)){
+       document.getElementById("winner").style.display =""
+    } else if (gameBoard.match(winO)){
+        document.getElementById("winner").style.display =""
+    }
 }
+
+console.log(gameBoard)
