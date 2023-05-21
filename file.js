b4342fc0c5cecbc12ae14c93216198fdf2a9f4b2
Maybol283 <126118241+Maybol283@users.noreply.github.com>
2023-05-21 12:48:20 +0900

    Added Start Menu
    Draw condition needs fixing

--- file.js
+++ file.js
@@ -1,5 +1,5 @@
 
-
+//Inital gameBoard state
 let gameBoard = [
 "U", "U", "U",
 "U", "U", "U",
@@ -15,7 +15,7 @@ for (let i = 1; i <= 9; i++) {
 
 let playersTurn = 0
 
-//Changes the tile to either X or O
+//Changes the tile to either X or O and updates players turn
 const  makeMove = tileIndex => {
     let tile = document.getElementById(tileIndex)
    
@@ -32,17 +32,42 @@ const  makeMove = tileIndex => {
  }
 
  
+//Win checker Regex's
+ winX = /^X{3}|...X{3}...|......X{3}|(X..){3}|.X..X..X.|(..X){3}|X...X...X|..X.X.X../gm
+ winO = /^O{3}|...O{3}...|......X{3}|(O..){3}|.O..O..O.|(..O){3}|O...O...O|..O.O.O../gm
+ Draw =/U/gmi
 
- winX = /^X{3}|(...)X{3}|(X..){3}|.X..X..X.|(..X){3}|X...X...X|..X.X.X../gm
- winO = /^O{3}|(...)O{3}|(O..){3}|.O..O..O.|(..O){3}|O...O...O|..O.O.O../gm
-
+ //Win checker function
   function checkWinner () {
     let boardCheck = gameBoard.join("");
+
     if (winX.test(boardCheck)){
-       document.getElementById("winner").style.color = "green"
+      document.getElementById("winner").style.visibility = "visible";
+      document.getElementById("winner").innerHTML = "Player 1 Wins!";
+      document.getElementById("winner").style.color = "green";
+      setTimeout(menuOpen, 1000);
+
     } else if (winO.test(boardCheck)){
-        document.getElementById("winner").style.color = "black"
+      document.getElementById("winner").style.visibility = "visible";
+      document.getElementById("winner").innerHTML = "Player 2 Wins!";
+        document.getElementById("winner").style.color = "black";
+        setTimeout(menuOpen, 1000);
+
+    } else if (!Draw.test(boardCheck)){
+      document.getElementById("winner").style.visibility = "visible";
+      document.getElementById("winner").innerHTML = "Draw";
+        document.getElementById("winner").style.color = "pink";
+        setTimeout(menuOpen, 1000);
     }
+    
 }
 
-
+//Start game button
+function startGame() {
+  document.querySelector(".Menu").style.visibility = "hidden";
+}
+//Re-Open the menu
+function menuOpen() {
+  //document.querySelector(".Menu").style.visibility = "visible";
+  //location.reload();
+}
