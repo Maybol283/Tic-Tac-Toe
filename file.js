deec7e3f0171cb724ad0acc10affa2318698c7ca
Maybol283 <126118241+Maybol283@users.noreply.github.com>
2023-05-25 08:43:29 +0900

    Scoreboard Updates/ Need to fix board reset

--- file.js
+++ file.js
@@ -6,13 +6,19 @@ let gameBoard = [
 "U", "U", "U",
 ];
 
+
 // Creates tiles 1 - 9
 for (let i = 1; i <= 9; i++) {
     document.getElementById(i).addEventListener("click", function () {
       makeMove(i);
+
     });
   }
+//Player Score
+let sOne = 0;
+let sTwo = 0;
 
+//Player turn tracker
 let playersTurn = 0
 
 //Changes the tile to either X or O and updates players turn
@@ -44,19 +50,23 @@ const  makeMove = tileIndex => {
     if (winX.test(boardCheck)){
       document.getElementById("winner").style.visibility = "visible";
       document.getElementById("winner").innerHTML = "Player 1 Wins!";
-      document.getElementById("winner").style.color = "var(--c3)";
+      document.getElementById("winner").style.color = "var(--c2)";
+      sOne ++;
+      document.getElementById("scoreOne").innerHTML = sOne;
       setTimeout(menuOpen, 1000);
 
     } else if (winO.test(boardCheck)){
       document.getElementById("winner").style.visibility = "visible";
       document.getElementById("winner").innerHTML = "Player 2 Wins!";
-        document.getElementById("winner").style.color = "var(--c3)";
+        document.getElementById("winner").style.color = "var(--c2)";
+        sTwo ++;
+        document.getElementById("scoreTwo").innerHTML = sTwo;
         setTimeout(menuOpen, 1000);
 
     } else if (Draw.test(boardCheck) && boardCheck.length == 9) {
       document.getElementById("winner").style.visibility = "visible";
       document.getElementById("winner").innerHTML = "Draw";
-        document.getElementById("winner").style.color = "pink";
+        document.getElementById("winner").style.color = "(--c2)";
         setTimeout(menuOpen, 1000);
     }
     
@@ -70,5 +80,7 @@ function startGame() {
 //Re-Open the menu
 function menuOpen() {
   document.querySelector(".Menu").style.visibility = "visible";
-  location.reload();
+ let tList = document.querySelectorAll(".tile");
+ tList.forEach(t => t.innerHTML ="");
+ gameBoard.forEach(i => gameBoard[i] = "U");
 }
